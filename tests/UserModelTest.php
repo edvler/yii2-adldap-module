09a8a6d7cf09afada5d9221fb61f7deee22d42b7
edvler <edvler@edvler-blog.de>
2017-09-12 11:39:59 +0200

    Added more descriptions

--- tests/UserModelTest.php
+++ tests/UserModelTest.php
@@ -3,33 +3,17 @@
 require_once(__DIR__ . '/../../../../models/LoginForm.php');
 use app\models\LoginForm;
 
-/*
- * This is the testclass for the Edvlerblog\Adldap2\SimpleUsageTest (file yii2-adldap-module/src/Adldap2Wrapper.php)
- * 
- * For a successfull test there are some prerequisites.
- * In TestCase.php
- *  - Change the configuration to your Active Directory
- * 
- * In Active Directory:
- *  - Create a user with
- *      * First Name (givenname): Yii2
- *      * Surname (sn): Testuser
- *      * Login Name (sAMAccountname): yii2testuser
- *      * Password: TestTest123
- *      * Make the user member of a group with the name yii2_example_group
- */
 class UserModelTest extends TestCase
 {
     private $testUsername = 'yii2testuser';
     private $testPassword = 'TestTest123';
     private $testGivenName = 'Yii2';
-    private $testSurName = 'Testuser';
     private $testGroupName = 'yii2_example_group';
     private $testNestedGroupName = 'yii2_nested_group';
     
     private $testDisabledUser = 'yii2testuserdis';
     
-    //After each test clea
+    //After each test clear
     public function tearDown() {
         \Yii::$app->db->createCommand()->truncateTable("auth_assignment")->execute();
         \Yii::$app->db->createCommand()->truncateTable("user")->execute();
@@ -55,26 +39,26 @@ class UserModelTest extends TestCase
     
     public function testQueryLdapUserGroupMembership() {
         $groups = $this->getNewUserModel()->getGroupsAssignedInLdap();
-        $this->assertContains($this->testGroupName, $groups);
+        $this->assertContains($this->testGroupName, $groups,'Group ' . $this->testGroupName . ' is missing!');
     }
     
     public function testFindByUsernameDisabledUser() {
         $userObject = Edvlerblog\Adldap2\model\UserDbLdap::findByUsername($this->testDisabledUser);
 
-        $this->assertNull($userObject);
+        $this->assertNull($userObject,'A disabled user cannot be returned by findByUsername');
     }
     
     public function testFindByUsername() {
         $userObject = Edvlerblog\Adldap2\model\UserDbLdap::findByUsername($this->testUsername);
         $userName = $userObject->queryLdapUserObject()['attributes']['samaccountname'][0];
         
-        $this->assertEquals($userName,$this->testUsername);
+        $this->assertEquals($userName,$this->testUsername,'No correct instance of the test user ' . $this->testUsername . ' returned by findByUsername');
     }     
     
     public function testFindIdentityNotExistingUserId() {
         $userObject = Edvlerblog\Adldap2\model\UserDbLdap::findIdentity(9999);
 
-        $this->assertNull($userObject);
+        $this->assertNull($userObject,'Not existing identity cannot be found!');
     }
     
     public function testFindIdentityWithId() {
@@ -83,7 +67,7 @@ class UserModelTest extends TestCase
 
         $userName = $userObjectById->queryLdapUserObject()['attributes']['samaccountname'][0];
         
-        $this->assertEquals($userName,$this->testUsername);
+        $this->assertEquals($userName,$this->testUsername,'No correct instance of the test user ' . $this->testUsername . ' returned by findByUsername');
     }
     
     public function testUpdateAccountStatus() {
@@ -91,24 +75,24 @@ class UserModelTest extends TestCase
         $userObject->status = Edvlerblog\Adldap2\model\UserDbLdap::STATUS_DISABLED;
         $userObject->save();
         
-        $this->assertEquals($userObject->status,Edvlerblog\Adldap2\model\UserDbLdap::STATUS_DISABLED);
+        $this->assertEquals($userObject->status,Edvlerblog\Adldap2\model\UserDbLdap::STATUS_DISABLED, 'User should be disabled.');
         $userObject->updateAccountStatus();
         
-        $this->assertEquals($userObject->status,Edvlerblog\Adldap2\model\UserDbLdap::STATUS_ENABLED);
+        $this->assertEquals($userObject->status,Edvlerblog\Adldap2\model\UserDbLdap::STATUS_ENABLED, 'User should be reenabled after updateAccountStatus.');
     }
 
     public function testCheckAllowedToLoginWithNullUser() {
         $userObject = Edvlerblog\Adldap2\model\UserDbLdap::findByUsername('NOTEXISTINGACCOUNT');
         $userObject2 = Edvlerblog\Adldap2\model\UserDbLdap::checkAllowedToLogin($userObject);
         
-        $this->assertEquals($userObject,$userObject2);
+        $this->assertEquals($userObject,$userObject2, 'The object returned for a successfull login by checkAllowedToLogin has to be euqal to the instance given as parameter');
     }   
     
     public function testCheckAllowedToLogin() {
         $userObject = Edvlerblog\Adldap2\model\UserDbLdap::findByUsername($this->testUsername);
         $userObject2 = Edvlerblog\Adldap2\model\UserDbLdap::checkAllowedToLogin($userObject);
         
-        $this->assertEquals($userObject,$userObject2);
+        $this->assertEquals($userObject,$userObject2, 'The object returned for a successfull login by checkAllowedToLogin has to be euqal to the instance given as parameter');
     }
     
     public function testUpdateGroupAssignment() {
@@ -116,8 +100,8 @@ class UserModelTest extends TestCase
         $yiiRolesAssignedToUser = Yii::$app->authManager->getRolesByUser($userObject->getId()); //Get all roles assigned to user
         
         //User has only group yii2_example_group assinged.
-        $this->assertArrayHasKey($this->testGroupName,$yiiRolesAssignedToUser);
-        $this->assertArrayNotHasKey($this->testNestedGroupName,$yiiRolesAssignedToUser);
+        $this->assertArrayHasKey($this->testGroupName,$yiiRolesAssignedToUser,'Role ' . $this->testGroupName . ' has to be assigned.');
+        $this->assertArrayNotHasKey($this->testNestedGroupName,$yiiRolesAssignedToUser,'Nested group ' . $this->testNestedGroupName . ' has NOT to be assigned.');
         
         //Search for nested groups
         $userObject->setIndividualGroupAssignmentOptions(
@@ -128,29 +112,35 @@ class UserModelTest extends TestCase
         //Nested group cannot bes assigned to user beacause no role in yii2 exists.
         $groupsPossible = $userObject->getGroupsAssignedInLdap();
         
-        $this->assertContains($this->testGroupName,$groupsPossible);
-        $this->assertContains($this->testNestedGroupName,$groupsPossible);
+        $this->assertContains($this->testGroupName,$groupsPossible,'Group ' . $this->testGroupName . ' has to be found in AD.');
+        $this->assertContains($this->testNestedGroupName,$groupsPossible,'Nested group ' . $this->testNestedGroupName . ' has to be found in AD.');
     }    
     
     public function testUserTestLogin() {
         $model = new LoginForm();
         
+        //Test a real login
         $this->assertTrue($model->load(['LoginForm'=> ['username' => $this->testUsername, 'password' => $this->testPassword]]),'Load data into model failed');
         $this->assertTrue($model->login(),'Login with LDAP failed');
-        $this->assertFalse(Yii::$app->user->isGuest);
-
+        $this->assertFalse(Yii::$app->user->isGuest,'User is not logged in');
+        
+        //Save id
         $idOfUser = Yii::$app->user->getId();      
         
+        //Simulate a request after a successfull Login
         $userObjectById = Edvlerblog\Adldap2\model\UserDbLdap::findIdentity($idOfUser);
         $userName = $userObjectById->queryLdapUserObject()['attributes']['samaccountname'][0];
-        $this->assertEquals($userName,$this->testUsername);
+        $this->assertEquals($userName,$this->testUsername,'No correct instance of the test user ' . $this->testUsername . ' returned by queryLdapUserObject');
         
+        //Logout
         Yii::$app->user->logout();
-        $this->assertTrue(Yii::$app->user->isGuest);
+        $this->assertTrue(Yii::$app->user->isGuest,'User is not a guest');
         
+        //Try a second login
         $this->assertTrue($model->load(['LoginForm'=> ['username' => $this->testUsername, 'password' => $this->testPassword]]),'Load data into model failed');
-        $this->assertTrue($model->login(),'Login with LDAP failed');        
+        $this->assertTrue($model->login(),'Login with LDAP failed');
         
-        $this->sendtoStdErr(Yii::$app->user->identity->queryLdapUserObject()['attributes']['samaccountname'][0]);
+        //Try permissions
+        $this->assertTrue(Yii::$app->user->can('permissionDisplayDetailedAbout'),'Permission cannot be found');
     }
 }
